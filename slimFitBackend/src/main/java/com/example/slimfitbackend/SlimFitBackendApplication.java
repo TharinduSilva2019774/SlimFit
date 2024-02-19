5f279b6e5c8ebc815f0b4b56fa554adcb1249ed0
Tharindu Silva <tsembakutti@gmail.com>
2024-02-19 13:01:59 +0000

    first commit

---
+++ slimFitBackend/src/main/java/com/example/slimfitbackend/SlimFitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.slimfitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlimFitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlimFitBackendApplication.class, args);
+	}
+
+}
