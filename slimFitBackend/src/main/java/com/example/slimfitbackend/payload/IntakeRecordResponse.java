485145a7902f8a2bd1d0de5b43c26c9d84e61316
Tharindu Silva <tsembakutti@gmail.com>
2024-04-22 14:38:47 +0100

    ok

--- slimFitBackend/src/main/java/com/example/slimfitbackend/payload/IntakeRecordResponse.java
+++ slimFitBackend/src/main/java/com/example/slimfitbackend/payload/IntakeRecordResponse.java
@@ -19,43 +19,4 @@ public class IntakeRecordResponse {
 
     private long meal;
 
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public long getCalorieCount() {
-        return calorieCount;
-    }
-
-    public void setCalorieCount(long calorieCount) {
-        this.calorieCount = calorieCount;
-    }
-
-    public String getMealName() {
-        return MealName;
-    }
-
-    public void setMealName(String mealName) {
-        MealName = mealName;
-    }
-
-    public String getNote() {
-        return note;
-    }
-
-    public void setNote(String note) {
-        this.note = note;
-    }
-
-    public long getMeal() {
-        return meal;
-    }
-
-    public void setMeal(long meal) {
-        this.meal = meal;
-    }
 }
